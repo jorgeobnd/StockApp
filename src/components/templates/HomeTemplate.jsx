9bac7882c9a56e29e699504eaa208d0a93f38ebe
jorgeobnd <ryuuzaki.dev@gmail.com>
2026-03-03 18:02:18 -0600

    create Home files

---
+++ src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function HomeTemplate() {
+  return (
+    <Container>
+      <div>HomeTemplate</div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+`;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { HomeTemplate } from "../index";
+
+export default function Home() {
+  return <HomeTemplate />;
+}
